fd36d640aa191ea768df42eb51ddb1f73c638bfa
khaledabdalslam99 <khaledabdalslam99@gmail.com>
2024-10-21 04:09:07 -0400

    navbar component created and styled successfully

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        <div className='navbar color-mode'>
+            <img className='navbar-logo' src='/images/logo.png' />
+            <div className='navbar-search'>
+                <input type='text' placeholder='Search Games ..' />
+                <img src='/icons/search.svg' />
+            </div>
+            <div className='navbar-darkmode'>
+                <label className="switch">
+                    <input type="checkbox" />
+                    <span className="slider round"></span>
+                </label>
+                <p>Dark mode</p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Index from './pages/Index/Index';
+import Home from './pages/Home/Home';
 
+import './index.css'
+const router = createBrowserRouter(
+  [
+    {
+      path:'/',
+      element: <Index />,
+      children: [
+        {
+          path: '',
+          element: <Home />,
+          children: [
+            {
+              path:'games',
+              element: <></>
+            },
+            {
+              path:'/games/:id',
+              element:<></>
+            }
+          ]
+        }
+
+      ]
+    }
+  ]
+)
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
