5f4caf68d1a58f7d157ec7ed328ca4af9e3e41ea
khaledabdalslam99 <khaledabdalslam99@gmail.com>
2024-10-24 07:50:40 -0400

    test

--- src/pages/Landingpage/Landingpage.tsx
+++ src/pages/Landingpage/Landingpage.tsx
@@ -10,7 +10,7 @@ const Landingpage = () => {
       <div className='landingpage-header'>
       </div>
       <div className='landingpage-text'>
-        <h1 className='fluorite-gradient'>Welcome to Game Hub test deployment</h1>
+        <h1 className='fluorite-gradient'>Welcome to Game Hub test test test</h1>
         <p>Explore and experience a new world of gaming</p>
         <button className='' onClick={()=>navigate('/4')}>Start your journey</button>
       </div>
