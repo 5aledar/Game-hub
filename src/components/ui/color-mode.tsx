a4d43e2920dafac97852a98d65f6fca7b07df2f6
khaledabdalslam99 <khaledabdalslam99@gmail.com>
2024-11-07 06:38:08 -0500

    push to deplpy

--- src/components/ui/color-mode.tsx
+++ src/components/ui/color-mode.tsx
@@ -3,13 +3,11 @@
 import type { IconButtonProps } from "@chakra-ui/react"
 import { ClientOnly, IconButton, Skeleton } from "@chakra-ui/react"
 import { ThemeProvider, useTheme } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
+import type { ThemeProviderProps } from "next-themes/dist/types"
 import { forwardRef } from "react"
 import { LuMoon, LuSun } from "react-icons/lu"
 
-export interface ColorModeProviderProps extends ThemeProviderProps {}
-
-export function ColorModeProvider(props: ColorModeProviderProps) {
+export function ColorModeProvider(props: ThemeProviderProps) {
   return (
     <ThemeProvider attribute="class" disableTransitionOnChange {...props} />
   )
